57537683db781f3a440663e4ef46ccb26722c399
Said Akça <56161195+akcasaid@users.noreply.github.com>
2024-02-05 17:04:44 +0300

    Add files via upload

---
+++ bosluk_tire.py
@@ -0,0 +1,13 @@
+'''
+, verilen bir dizeyi boşluk karakterine göre bölmek ve ardından bu kelimeleri bir tire "-" ile birleştirmek
+'''
+
+def split_and_join(line):
+    words= line.split(" ") # Önce dizeyi boşluklara göre ayırıyoruz.
+    join_line = "-".join(words)  # Sonra kelimeleri bir tire ile birleştiriyoruz.
+    return join_line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
